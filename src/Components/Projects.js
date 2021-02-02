de3990e34525e85f3e369867ce35ad534058175d
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-02-02 00:10:52 -0600

    imported thumbnail comp, returned in jsx

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,11 +1,20 @@
 import React from "react"
+import Thumbnail from './Thumbnail';
+import '../App.css';
  
-function NewComponent(props) {
+function Projects(props) {
   return (
+    // Render a Thumbnail component
     <div>
-      <h1>New Component</h1>
+      <h1>Projects</h1>
+      <Thumbnail
+        link=""
+        image=""
+        title=""
+        category=""
+      />
     </div>
   )
 }
  
-export default NewComponent;
+export default Projects;
